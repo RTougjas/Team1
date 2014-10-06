0fe9f4dcc0508f847e7844d03b1b31196eba82e7
Rain <Rrain@rains-MacBook-Pro.local>
2014-10-06 06:34:38 +0300

    test
    ?

---
+++ POS/src/ee/ut/math/tvt/team1/Intro.java
@@ -0,0 +1,12 @@
+package ee.ut.math.tvt.team1;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("LAMMAS");
+
+	}
+
+}
---
+++ POS/src/ee/ut/math/tvt/team1/IntroUI.java
@@ -0,0 +1,5 @@
+package ee.ut.math.tvt.team1;
+
+public class IntroUI {
+
+}
