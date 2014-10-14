edafed117d0e2d43e224ff08e07556cc6aa61ece
Rain <raintoug@ut.ee>
2014-10-14 20:23:32 +0300

    code more readable

--- src/ee/ut/math/tvt/team1/Intro.java
+++ src/ee/ut/math/tvt/team1/Intro.java
@@ -1,52 +1,10 @@
 package ee.ut.math.tvt.team1;
 
-import java.awt.Image;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Properties;
+public class Intro {
 
-import javax.swing.text.html.ImageView;
-
-public class Intro { //#YoloSwaggins
-	
-	private String teamName;
-	private String teamLeader;
-	private String teamLeaderEmail;
-	private String[] teamMembers = new String[4];
-	private ImageView logo;
-	private String versionNumber;
-	
-	public static String readProperty(File file, String key) {
-		
-		Properties properties = new Properties();
-		InputStream input;
-		String value = "";
-		
-		try {
-			input = new FileInputStream(file);
-			properties.load(input);
-			value = properties.getProperty(key);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return value;
-	}
-	
 	public static void main(String[] args) {
 		
-		
-		File appProp = new File("application.properties");
-		File verProp = new File("version.properties");
-		
+		IntroUI intro = new IntroUI("application.properties", "version.properties");
 		
 
 	}
