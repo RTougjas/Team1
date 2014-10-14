51234cb89a50e5b6177db59f134109da5bf18e85
Rain <raintoug@ut.ee>
2014-10-14 23:16:49 +0300

    ma ei tea...

--- TVT/src/ee/ut/math/tvt/team1/Intro.java
+++ TVT/src/ee/ut/math/tvt/team1/Intro.java
@@ -1,11 +1,20 @@
 package ee.ut.math.tvt.team1;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class Intro {
 
+	static Logger logger = Logger.getLogger(Intro.class);
+	
 	public static void main(String[] args) {
 		
 		IntroUI intro = new IntroUI("application.properties", "version.properties");
-		System.out.println("hello");
+		
+		BasicConfigurator.configure();
+
+	    logger.info(Intro.class.getSimpleName() + " Window has opened");
+		
 		
 
 	}
