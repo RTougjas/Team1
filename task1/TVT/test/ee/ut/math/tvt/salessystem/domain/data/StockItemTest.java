2da03163d23c9ee0fb687e484d984a86bf3b63ae
Tarinu <kaarel06@gmail.com>
2014-11-18 15:01:05 +0200

    StockItemTest peaks olema valmis

--- task1/TVT/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
+++ task1/TVT/test/ee/ut/math/tvt/salessystem/domain/data/StockItemTest.java
@@ -1,17 +1,30 @@
 package ee.ut.math.tvt.salessystem.domain.data;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class StockItemTest {
 	
-	@Test
-	public void testClone(){
-		//TODO
+	private StockItem item1;
+	
+	@Before
+	public void setUp(){
+		item1 = new StockItem((long) 10, "Test", "StockItem for testing", 10.0, 10);
 	}
 	
 	@Test
+	public void testClone(){
+		assertEquals(item1.clone().toString(), item1.toString());
+	}
+	
+	@Test(expected=RuntimeException.class)
 	public void testGetColumn(){
-		//TODO
+		assertEquals(item1.getColumn(0), item1.getId());
+		assertEquals(item1.getColumn(1), item1.getName());
+		assertEquals(item1.getColumn(2), item1.getPrice());
+		assertEquals(item1.getColumn(3), item1.getQuantity());
+		item1.getColumn(4);
 	}
 }
